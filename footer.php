685546b98a87a7e29c3e6b5af32c3bd8deba12e5
badis <badisthecreator@gmail.com>
2026-05-18 11:30:19 +0100

    static files

--- footer.php
+++ footer.php
@@ -11,11 +11,11 @@
       </div>
 
       <div class="box">
-         <h3>extra links</h3>
-         <a href="login.php">login</a>
-         <a href="register.php">register</a>
-         <a href="cart.php">cart</a>
-         <a href="orders.php">orders</a>
+            <h3>extra links</h3>
+            <a href="login.php">login</a>
+            <a href="register.php">register</a>
+            <a href="cart.php">cart</a>
+            <a href="orders.php">orders</a>
       </div>
 
       <div class="box">
