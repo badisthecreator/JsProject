9c2a13553ebf2151f663839aa66e222ca01c750b
Youssef <youssef@email.com>
2026-05-18 11:28:38 +0100

    addintions

--- admin_header.php
+++ admin_header.php
@@ -15,24 +15,24 @@ if (isset($message)) {
     <div class="flex">
         <a href="admin_page.php" class="logo">Admin<span>Panel</span></a>
 
-        <nav class="navbar">
-            <a href="admin_page.php">home</a>
-            <a href="admin_products.php">products</a>
-            <a href="admin_orders.php">orders</a>
-            <a href="admin_users.php">users</a>
-            <a href="admin_contacts.php">messages</a>
-        </nav>
+            <nav class="navbar">
+                <a href="admin_page.php">home</a>
+                <a href="admin_products.php">products</a>
+                <a href="admin_orders.php">orders</a>
+                <a href="admin_users.php">users</a>
+                <a href="admin_contacts.php">messages</a>
+            </nav>
 
         <div class="icons">
             <div id="menu-btn" class="fas fa-bars"></div>
             <div id="user-btn" class="fas fa-user"></div>
         </div>
 
-        <div class="account-box">
-            <p>username : <span><?php echo htmlspecialchars($_SESSION['admin_name'], ENT_QUOTES, 'UTF-8'); ?></span></p>
-            <p>email : <span><?php echo htmlspecialchars($_SESSION['admin_email'], ENT_QUOTES, 'UTF-8'); ?></span></p>
-            <a href="logout.php" class="delete-btn">logout</a>
-            <div>new <a href="login.php">login</a> | <a href="register.php">register</a></div>
-        </div>
+            <div class="account-box">
+                <p>username : <span><?php echo htmlspecialchars($_SESSION['admin_name'], ENT_QUOTES, 'UTF-8'); ?></span></p>
+                <p>email : <span><?php echo htmlspecialchars($_SESSION['admin_email'], ENT_QUOTES, 'UTF-8'); ?></span></p>
+                <a href="logout.php" class="delete-btn">logout</a>
+                <div>new <a href="login.php">login</a> | <a href="register.php">register</a></div>
+            </div>
     </div>
 </header>
