496ae44e5ce9432e02c2594353af28eb5f9449c2
Insaf <insaf@email.com>
2026-05-18 11:26:54 +0100

    authentification

--- logout.php
+++ logout.php
@@ -2,9 +2,11 @@
 
 include 'config.php';
 
+
 session_start();
 session_unset();
 session_destroy();
+
 
 header('location:login.php');
 
--- register.php
+++ register.php
@@ -37,19 +37,19 @@ if(isset($_POST['submit'])){
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>register</title>
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>register</title>
 
-   <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+      <!-- font awesome cdn link  -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+      <!-- custom css file link  -->
+      <link rel="stylesheet" href="css/style.css">
 
-</head>
+   </head>
 <body>
 
 
@@ -69,34 +69,34 @@ if(isset($message)){
    
 <div class="form-container">
 
-   <form action="" method="post">
-      <h3>register now</h3>
-      <input type="text" name="name" placeholder="enter your name" required class="box">
-      <input type="email" name="email" placeholder="enter your email" required class="box">
-      <input type="password" name="password" placeholder="enter your password" required class="box">
-      <input type="password" name="cpassword" placeholder="confirm your password" required class="box">
-      <select name="user_type" id="user-type" class="box">
-         <option value="user">user</option>
-         <option value="admin">admin</option>
-         
-      </select>
-      <input type="admintext" name="adminSecret" id="admin-inputText" placeholder="Admin input"  class="box" style="display: none;">
-      <input type="submit" name="submit" value="register now" class="btn">
-      <p>already have an account? <a href="login.php">login now</a></p>
-   </form>
-   <script>
-    const userTypeSelect = document.getElementById("user-type");
-    const adminInput = document.getElementById("admin-inputText");
+      <form action="" method="post">
+         <h3>register now</h3>
+         <input type="text" name="name" placeholder="enter your name" required class="box">
+         <input type="email" name="email" placeholder="enter your email" required class="box">
+         <input type="password" name="password" placeholder="enter your password" required class="box">
+         <input type="password" name="cpassword" placeholder="confirm your password" required class="box">
+         <select name="user_type" id="user-type" class="box">
+            <option value="user">user</option>
+            <option value="admin">admin</option>
+            
+         </select>
+         <input type="admintext" name="adminSecret" id="admin-inputText" placeholder="Admin input"  class="box" style="display: none;">
+         <input type="submit" name="submit" value="register now" class="btn">
+         <p>already have an account? <a href="login.php">login now</a></p>
+      </form>
+      <script>
+      const userTypeSelect = document.getElementById("user-type");
+      const adminInput = document.getElementById("admin-inputText");
 
-    userTypeSelect.addEventListener("change", (event) => {
-      if (event.target.value === "admin") {
-        adminInput.style.display = "block";
-      } else {
-        adminInput.style.display = "none";
-        adminInput.value = ""; // Clear the input field
-      }
-    });
-  </script>
+      userTypeSelect.addEventListener("change", (event) => {
+         if (event.target.value === "admin") {
+         adminInput.style.display = "block";
+         } else {
+         adminInput.style.display = "none";
+         adminInput.value = ""; // Clear the input field
+         }
+      });
+   </script>
 
 </div>
 
